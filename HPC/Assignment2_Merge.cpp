6db1103a07881064731af0f4428a2a91ee0468c7
Mihir <34931141+MihirDesh@users.noreply.github.com>
2025-05-04 13:51:41 +0000

    final merge sort code

--- HPC/Assignment2_Merge.cpp
+++ HPC/Assignment2_Merge.cpp
@@ -4,63 +4,33 @@
 using namespace std;
 
 void merge(int arr[], int low, int mid, int high) {
-    // Create arrays of left and right partititons
     int n1 = mid - low + 1;
     int n2 = high - mid;
 
-    int left[n1];
-    int right[n2];
-    
-    // Copy all left elements
+    int left[n1], right[n2];
     for (int i = 0; i < n1; i++) left[i] = arr[low + i];
-    
-    // Copy all right elements
     for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
-    
-    // Compare and place elements
+
     int i = 0, j = 0, k = low;
-
     while (i < n1 && j < n2) {
-        if (left[i] <= right[j]){
-            arr[k] = left[i];
-            i++;
-        } 
-        else{
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
+        if (left[i] <= right[j]) arr[k++] = left[i++];
+        else arr[k++] = right[j++];
     }
 
-    // If any elements are left out
-    while (i < n1) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = right[j];
-        j++;
-        k++;
-    }
+    while (i < n1) arr[k++] = left[i++];
+    while (j < n2) arr[k++] = right[j++];
 }
 
 void parallelMergeSort(int arr[], int low, int high) {
     if (low < high) {
         int mid = (low + high) / 2;
-
         #pragma omp parallel sections
         {
             #pragma omp section
-            {
-                parallelMergeSort(arr, low, mid);
-            }
+            parallelMergeSort(arr, low, mid);
 
             #pragma omp section
-            {
-                parallelMergeSort(arr, mid + 1, high);
-            }
+            parallelMergeSort(arr, mid + 1, high);
         }
         merge(arr, low, mid, high);
     }
@@ -75,28 +45,40 @@ void mergeSort(int arr[], int low, int high) {
     }
 }
 
+// Function to print the array
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << "\n";
+}
+
 int main() {
     int n = 10;
     int arr[n];
     double start_time, end_time;
 
-    // Create an array with numbers starting from n to 1.
     for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
     
-    // Measure Sequential Time
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    // Sequential Merge Sort
     start_time = omp_get_wtime(); 
     mergeSort(arr, 0, n - 1);
     end_time = omp_get_wtime(); 
+    cout << "Sorted array using sequential merge sort: ";
+    printArray(arr, n);
     cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
 
     // Reset the array
     for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    //Measure Parallel time
+
+    // Parallel Merge Sort
     start_time = omp_get_wtime(); 
     parallelMergeSort(arr, 0, n - 1);
     end_time = omp_get_wtime(); 
-    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
-    
+    cout << "Sorted array using parallel merge sort: ";
+    printArray(arr, n);
+    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds\n";
+
     return 0;
 }
